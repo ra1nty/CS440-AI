a0c9162ce38815f59120df50e45a5e8ee209a2e4
Michael Hao <mxhao2@illinois.edu>
2015-09-17 12:26:48 -0400

    add bfs

--- mp1/bfs.py
+++ mp1/bfs.py
@@ -2,6 +2,7 @@ import sys
 import os
 from maze import Maze
 
-def BFS(parsedMaze, timeseries, startingNode){
-  
-}
+#def BFS(parsedMaze, timeseries, startingNode):
+
+def main():
+    print "test"
