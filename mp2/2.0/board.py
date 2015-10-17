b5dfa672b1d99c851d15c67a4a830e842f1f4c84
Michael Hao <mxhao2@illinois.edu>
2015-10-17 16:05:33 -0400

    semicolons

--- mp2/2.0/board.py
+++ mp2/2.0/board.py
@@ -10,12 +10,12 @@ class board:
   GREEN = 2;
   BOARD_SIZE = 6;
   # board values for each square
-  vals = [[0 for x in range(BOARD_SIZE)] for x in range(BOARD_SIZE)]
+  vals = [[0 for x in range(BOARD_SIZE)] for x in range(BOARD_SIZE)];
   # who occupies each square on the board
   # 0: none
   # 1: blue
   # 2: green
-  occupant = [[0 for x in range(BOARD_SIZE)] for x in range(BOARD_SIZE)] 
+  occupant = [[0 for x in range(BOARD_SIZE)] for x in range(BOARD_SIZE)]; 
 
 
   # initializes 6 by 6 board with random values
@@ -24,7 +24,7 @@ class board:
     i = 0;
     for x in range(0, self.BOARD_SIZE):
       for y in range (0, self.BOARD_SIZE):
-        self.vals[y][x] = randint(1,99)
+        self.vals[y][x] = randint(1,99);
         self.occupant[y][x] = 0;
 
 
@@ -33,18 +33,18 @@ class board:
     for y in range(0, self.BOARD_SIZE):
       for x in range(0, self.BOARD_SIZE):
         if(self.vals[y][x] < 10):
-          temp = temp + str(self.vals[y][x]) + "  "
+          temp = temp + str(self.vals[y][x]) + "  ";
         else:
-          temp = temp + str(self.vals[y][x]) + " "
-      print temp
-      temp = ""
+          temp = temp + str(self.vals[y][x]) + " ";
+      print temp;
+      temp = "";
 
 
   def nodeVal(self, x, y):
     return self.vals[y][x];
 
   def nodeOccupant(self, x,y):
-    return self.occupant[y][x]
+    return self.occupant[y][x];
 
 
 
@@ -54,10 +54,10 @@ class board:
   # outputs -- node value if successful, -1 if failed
   def commandoParaDrop(self, player, x,y):
     if self.occupant[y][x] == 0:
-      self.occupant[y][x] = player 
-      return self.vals[y][x]
+      self.occupant[y][x] = player;
+      return self.vals[y][x];
     else:
-      return -1
+      return -1;
 
   # same as commandoParaDrop but does not actually populate board
   # inputs -- x, y : location on where to drop
@@ -65,9 +65,9 @@ class board:
   # outputs -- node value if successful, -1 if failed  
   def commandoParDropPeek(self, x,y):
     if self.occupant[y][x] == 0:
-      return self.vals[y][x]
+      return self.vals[y][x];
     else:
-      return -1
+      return -1;
 
 
 
@@ -76,32 +76,32 @@ class board:
   #           player =  green (2) or blue (1) 
   # outputs -- total values earned if successful, -1 if failed
   def m1DeathBlitz(self, player, x, y):
-    ret = 0
-    opposingPlayer = 0
+    ret = 0;
+    opposingPlayer = 0;
     if player == self.GREEN:
-      opposingPlayer = self.BLUE
+      opposingPlayer = self.BLUE;
     else:
-      opposingPlayer = self.GREEN
+      opposingPlayer = self.GREEN;
     # take initial spot
-    self.occupant[y][x] = player
+    self.occupant[y][x] = player;
 
     # check neighbors for opposing pieces and change them
     # right
     if x + 1 >= self.BOARD_SIZE:
       if self.occupant[y][x + 1] == opposingPlayer:
-        self.occupant[y][x + 1] = player
+        self.occupant[y][x + 1] = player;
     # left 
     if x - 1 < 0:
       if self.occupant[y][x - 1] == opposingPlayer:
-        self.occupant[y][x - 1] = player
+        self.occupant[y][x - 1] = player;
     # up 
     if y + 1 >= self.BOARD_SIZE:
       if self.occupant[y + 1][x] == opposingPlayer:
-        self.occupant[y + 1][x] = player
+        self.occupant[y + 1][x] = player;
     # down
     if y - 1 < 0:
       if self.occupant[y - 1][x] == opposingPlayer:
-        self.occupant[y - 1][x] = player
+        self.occupant[y - 1][x] = player;
 
 
 
@@ -120,18 +120,18 @@ class board:
     game_over = 1;
     blue_score = 0;
     green_score = 0;
-    ret = []
+    ret = [];
     for x in range(0, self.BOARD_SIZE):
       for y in range (0, self.BOARD_SIZE):
         if self.occupant[y][x] == self.BLUE:
-          blue_score += self.vals[y][x]
+          blue_score += self.vals[y][x];
         elif self.occupant[y][x] ==  self.GREEN:
-          green_score += self.vals[y][x]
+          green_score += self.vals[y][x];
         else:
-          game_over = 0
-    ret.append(game_over)
-    ret.append(blue_score)
-    ret.append(green_score)
+          game_over = 0;
+    ret.append(game_over);
+    ret.append(blue_score);
+    ret.append(green_score);
 
     return ret
 
--- mp2/2.0/playerminimax.py
+++ mp2/2.0/playerminimax.py
@@ -1,3 +1,7 @@
 
 class playerMiniMax:
-  
+  BLUE = 1;
+  GREEN = 2;
+  color = 0;
+  def __init__(self, color):
+    self.color = color;
