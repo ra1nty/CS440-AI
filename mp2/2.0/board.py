96624c16e9bba2c5d0397b2113fff5770976ddf6
Michael Hao <mxhao2@illinois.edu>
2015-10-17 12:58:35 -0400

    finish board implementation and begin minimax player

--- mp2/2.0/board.py
+++ mp2/2.0/board.py
@@ -3,6 +3,7 @@ import pdb
 import os
 from random import randint
 
+
 class board:
   NONE = 0;
   BLUE = 1;
@@ -18,7 +19,7 @@ class board:
 
 
   # initializes 6 by 6 board with random values
-
+  # initializes occupied board all to zero
   def __init__(self):
     i = 0;
     for x in range(0, self.BOARD_SIZE):
@@ -46,18 +47,94 @@ class board:
     return self.occupant[y][x]
 
 
+
   # executes commando para drop on board
   # inputs -- x, y : location on where to drop
   #           player =  green (2) or blue (1) 
   # outputs -- node value if successful, -1 if failed
   def commandoParaDrop(self, player, x,y):
-    if self.occupant[y][x] == 0
+    if self.occupant[y][x] == 0:
       self.occupant[y][x] = player 
       return self.vals[y][x]
-    else
+    else:
       return -1
 
+  # same as commandoParaDrop but does not actually populate board
+  # inputs -- x, y : location on where to drop
+  #           player =  green (2) or blue (1) 
+  # outputs -- node value if successful, -1 if failed  
+  def commandoParDropPeek(self, x,y):
+    if self.occupant[y][x] == 0:
+      return self.vals[y][x]
+    else:
+      return -1
+
+
+
+  # executes M1 Death Blitz on board
+  # inputs -- x, y : location on where to drop
+  #           player =  green (2) or blue (1) 
+  # outputs -- total values earned if successful, -1 if failed
   def m1DeathBlitz(self, player, x, y):
+    ret = 0
+    opposingPlayer = 0
+    if player == self.GREEN:
+      opposingPlayer = self.BLUE
+    else:
+      opposingPlayer = self.GREEN
+    # take initial spot
+    self.occupant[y][x] = player
+
+    # check neighbors for opposing pieces and change them
+    # right
+    if x + 1 >= self.BOARD_SIZE:
+      if self.occupant[y][x + 1] == opposingPlayer:
+        self.occupant[y][x + 1] = player
+    # left 
+    if x - 1 < 0:
+      if self.occupant[y][x - 1] == opposingPlayer:
+        self.occupant[y][x - 1] = player
+    # up 
+    if y + 1 >= self.BOARD_SIZE:
+      if self.occupant[y + 1][x] == opposingPlayer:
+        self.occupant[y + 1][x] = player
+    # down
+    if y - 1 < 0:
+      if self.occupant[y - 1][x] == opposingPlayer:
+        self.occupant[y - 1][x] = player
+
+
+
+
+
+
+
+
+  # gets current status of the board
+  # inputs -- NONE
+  # outputs -- list
+  #   index 0: whether or not game is over
+  #   index 1: blue score
+  #   index 2: green score
+  def getStatus(self):
+    game_over = 1;
+    blue_score = 0;
+    green_score = 0;
+    ret = []
+    for x in range(0, self.BOARD_SIZE):
+      for y in range (0, self.BOARD_SIZE):
+        if self.occupant[y][x] == self.BLUE:
+          blue_score += self.vals[y][x]
+        elif self.occupant[y][x] ==  self.GREEN:
+          green_score += self.vals[y][x]
+        else:
+          game_over = 0
+    ret.append(game_over)
+    ret.append(blue_score)
+    ret.append(green_score)
+
+    return ret
+
 
 
 
@@ -66,6 +143,7 @@ class board:
 def main():
   b = board()
   b.printBoard()
-    
+  status  = b.getStatus()
+  print status
 
 main()
