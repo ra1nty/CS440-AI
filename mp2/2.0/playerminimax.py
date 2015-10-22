dfac73bb2f078dcf6c5202a2abd41bba90cc57f3
Michael Hao <mxhao2@illinois.edu>
2015-10-22 12:55:11 -0400

    player minimax implementation

--- mp2/2.0/playerminimax.py
+++ mp2/2.0/playerminimax.py
@@ -1,31 +1,74 @@
 from board import board
 from node import stateNode
 
+# define blue as Maximizing player
+# define 
 class MiniMax:
-  BLUE = 1;
-  GREEN = 2;
+  BLUE = 1; # MAX, starts first
+  GREEN = 2; # MIN starts second 
   color = 0;
   COMMANDO = 3;
   M1 = 4;
   board;
 
-  def __init__(self, color, board):
+  def __init__(self, color):
     self.color = color;
-    self.board = board;
+  
 
+  def makeMove(self, curGame):
+
+
+
+  # recursive minimax algorithm
+  # inputs -- board: current state of the board
+  #        -- 
+  # outputs -- returns a list
+  # index 0: x position
+  # index 1: y position
+  # index 2: type of move
+  # index 3: corresponding score for that move
   def minimax(board, depth, color):
+    # initialize variables
+    bestScore = 0;
+    currentScore = 0;
+    if color == self.BLUE:
+      bestScore = -1;
+    else:
+      bestScore = 1; 
+    bestX = -1;
+    bestY = -1;
+    bestMove = -1;
+
+    # get all possible moves
+    possibleCommando = list();
+    possibleCommando = board.getCommandoPoints();
+    possibleM1 = list();
+    possibleM1 = board.getM1Points(color);
+
+
+    ret = list();
     status = board.getStatus();
+    # Game has reached terminal state
     if status[0] == 1
+      # Blue Wins 
       if status[1] > status[2]:
         return 1;
+      # Blue Loses
       elif status[1] < status[2]:
         return -1;
       else:
+      # Draw
         return 0;
+    # stopped expansion on a certain depth
     elif depth == 0:
-        return evalfn(board)
+
     else:
       bestScore = -1;
+
+      ret.append(bestX);
+      ret.append(bestY);
+      ret.append(bestMove);
+      ret.append(bestScore);
 
 
   def evalFn(board):
