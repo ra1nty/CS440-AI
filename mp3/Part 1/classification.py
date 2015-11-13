4f712a8b22e7b880cdf224b810f832af2d078d4c
Jeffrey Huang <jeffrey.huang977@gmail.com>
2015-11-12 19:57:47 -0600

    Finalized main test function

--- mp3/Part 1/classification.py
+++ mp3/Part 1/classification.py
@@ -5,6 +5,7 @@ import pdb
 
 class Classifier:
 
+    LEARN = 0
     edge = '+'
     body = '#'
     __imageN = 28
@@ -41,6 +42,9 @@ class Classifier:
         representation = [0] * self.__imageN * self.__imageN
         self.totalImages = 0
 
+        """
+        Go through every line in the image
+        """
         for line in images:
             for i in range(0, self.__imageN):
                 if not line[i] == ' ':
@@ -48,6 +52,9 @@ class Classifier:
 
             counter += 1
 
+            """
+            End of an image, store the current representation in the frequency table and reset everything
+            """
             if counter == self.__imageN:
                 counter = 0
                 nextNum = int(labels.pop(0))
@@ -65,6 +72,9 @@ class Classifier:
         self.smoothing()
         self.totalPixels = self.countPixels()
 
+    """
+    Deletes the current trained information and runs through the new training set
+    """
     def retrain(self, trainingImages, trainingLabels):
         self.likelyhoods = dict()
         self.classNum = dict()
@@ -78,9 +88,6 @@ class Classifier:
 
     def generateIndex(self, x, y):
         return y * self.__imageN + x
-
-    def chainRule(self, jointProbabilities):
-        pass
 
     def testRelaxed(self, trainingImages, trainingLabels, n, m):
         images = open(trainingImages, 'r')
@@ -170,11 +177,12 @@ class Classifier:
                 total += 1
                 counter = 0
 
-                for i in range(0, self.__imageN * self.__imageN):
-                    self.likelyhoods[actual][i] += representation[i]
+                if self.LEARN:
+                    for i in range(0, self.__imageN * self.__imageN):
+                        self.likelyhoods[actual][i] += representation[i]
 
-                self.classNum[actual] += 1
-                self.totalImages += 1
+                    self.classNum[actual] += 1
+                    self.totalImages += 1
 
                 representation = [0] * self.__imageN * self.__imageN
 
@@ -194,6 +202,9 @@ class Classifier:
 
     constant = 1
 
+    """
+    Smooth a single array
+    """
     def smoothArr(self, arr, classNum):
         constant = self.constant
 
@@ -205,6 +216,9 @@ class Classifier:
 
         return arr
 
+    """
+    Smooth the likelyhoods, called after the main training function
+    """
     def smoothing(self):
         constant = self.constant
         self.totalImages = 0
@@ -227,9 +241,30 @@ class Classifier:
     """
 
     def test(self, testImages, testLabels):
+
+        """
+        Setup variables
+        """
         images = open(testImages, 'r')
         labels = open(testLabels, 'r').read().split('\n')
         self.confusionMatrix = list()
+        PHighest = dict()
+        PLowest = dict()
+        highest = dict()
+        lowest = dict()
+        classificationRate = dict()
+        totalPerDigit = dict()
+        currCount = 0
+
+        """
+        Initial Setup Code
+        """
+
+        for i in range(0, len(self.likelyhoods.keys())):
+            PHighest[i] = -9999999999
+            PLowest[i] = 9999999999
+            classificationRate[i] = 0
+            totalPerDigit[i] = 0
 
         """
         Create the confusion matrix as an NxN array
@@ -270,6 +305,10 @@ class Classifier:
 
                     # For each pixel calculate the probability
                     for i in range(0, self.__imageN * self.__imageN):
+                        """
+                        If representation is 1 then do pixels/total
+                        otherwise do 1-pixels/total for the likelyhood that it didn't appear there
+                        """
                         if representation[i] == 1:
                             PixelP[i] = posterior[i]/(self.classNum[idx] + 0.0)
                         else:
@@ -296,29 +335,49 @@ class Classifier:
 
                 actual = int(labels.pop(0))
                 classClassified[actual] += 1
+
                 if idx == actual:
                     accuracy += 1
+                    classificationRate[actual] += 1
                 else:
                     self.confusionMatrix[actual][idx] += 1
 
+                """
+                Check if lower or higher than curr lowest/highest and then store
+                """
+                if testing[actual] < PLowest[actual]:
+                    PLowest[actual] = testing[actual]
+                    lowest[actual] = list(representation)
+                if testing[actual] > PHighest[actual]:
+                    PHighest[actual] = testing[actual]
+                    highest[actual] = list(representation)
 
                 """
                 Use the new data and learn from it to improve the future results
-                (Not sure if we should do this but this brought the accuracy up to around 70%)
+                Increased the accuracy up 1.5%
                 """
+                if self.LEARN:
+                    for i in range(0, self.__imageN * self.__imageN):
+                        self.likelyhoods[actual][i] += representation[i]
 
-                for i in range(0, self.__imageN * self.__imageN):
-                    self.likelyhoods[actual][i] += representation[i]
-
-                self.classNum[actual] += 1
-                self.totalImages += 1
+                    self.classNum[actual] += 1
+                    self.totalImages += 1
 
                 representation = [0] * self.__imageN * self.__imageN
                 total += 1
+                totalPerDigit[actual] += 1
 
+        """
+        Output of the function, displays the accuracies of everything
+        """
         # The accuracy of the predictions
-        print "Accuracy " + str(accuracy/(total + 0.0))
+        print "Accuracy " + str(accuracy/(total + 0.0)) + "\n"
 
+        print "Accuracy per digit"
+        for i in range(0, len(self.likelyhoods.keys())):
+            print str(i) + " | " + str(float(classificationRate[i]/(totalPerDigit[i] + 0.0)))
+
+        self.exportHighestLowest(PHighest, PLowest, highest, lowest)
         print "  ",
         for i in range(0, len(self.confusionMatrix)):
             print i,
@@ -331,6 +390,22 @@ class Classifier:
                 self.confusionMatrix[i][j] = round(self.confusionMatrix[i][j], 2)
 
         return self.confusionMatrix
+
+    def exportHighestLowest(self, PHighest, PLowest, highest, lowest):
+        high = open('highest.p', 'w')
+        low = open('lowest.p', 'w')
+
+        for i in range(0, len(self.likelyhoods.keys())):
+            high.write(str(PHighest[i]) + "\n")
+            low.write(str(PLowest[i]) + "\n")
+            counter = 0
+            for idx in range(0, self.__imageN * self.__imageN):
+                high.write(str(highest[i][idx]))
+                low.write(str(lowest[i][idx]))
+                counter += 1
+                if counter % self.__imageN == 0:
+                    high.write("\n")
+                    low.write("\n")
 
     def oddRatios(self, confusionMatrix):
         maxConfusion = list()
