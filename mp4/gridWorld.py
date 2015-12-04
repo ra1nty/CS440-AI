3e1d2ad86029483509096ed99d102f40e8258426
Jeffrey Huang <jeffrey.huang977@gmail.com>
2015-12-04 16:59:42 -0600

    Why doesn't this work

--- mp4/gridWorld.py
+++ mp4/gridWorld.py
@@ -4,6 +4,9 @@ from Queue import Queue
 from sets import Set
 import pdb
 import copy
+import valueIteration
+from reinforcementLearning import *
+from policyIteration import *
 
 mazeWorld = [[0, -1, 0, 0, 0, 0],
              [0, 0, 0, -999, -1, 0],
@@ -14,7 +17,6 @@ mazeWorld = [[0, -1, 0, 0, 0, 0],
 
 starting = (4, 1)
 
-policies = dict()
 intended = 0.8
 right = 0.1
 left = 0.1
@@ -26,121 +28,14 @@ LEFT = 1
 RIGHT = 3
 
 def main():
-    valueIteration()
+    print "~~~~~~~~~~ Value iteration rewards mean stop ~~~~~~~~~~\n\n"
+    valueIteration.valueIteration()
+    valueIteration.STOP = 0
+    print "\n\n~~~~~~~~~~ Value iteration rewards mean nothing ~~~~~~~~~~\n\n"
+    valueIteration.valueIteration()
 
-def valueIteration():
-    t = 1
-    values = list()
-    values.append(copy.deepcopy(mazeWorld))
-    moves = list()
-
-    while True:
-        terminals = list()
-        curr = copy.deepcopy(values[t - 1])
-        currMoves = copy.deepcopy(values[t-1])
-
-        for y in range(len(mazeWorld)):
-            for x in range(len(mazeWorld[y])):
-                if values[t-1][y][x] is not 0 and values[t-1][y][x] is not -999:
-                    terminals.append((y, x))
-
-        for terminal in terminals:
-            surroundingBlocks = getSurroundingBlocks(terminal)
-
-            for block in surroundingBlocks:
-                actions = getActions(block)
-                prev = values[t - 1]
-                maxAction = -9999
-                bestAction = -1
-
-                for actionSet in actions:
-                    total = 0.0
-
-                    for action in actionSet:
-                        y = action[0]
-                        x = action[1]
-
-                        if y < len(mazeWorld) and y >= 0 and x < len(mazeWorld[0]) and x >= 0:
-                            if not mazeWorld[y][x] == -999:
-                                total += (action[2] * (prev[action[0]][action[1]]))
-
-                    if total > maxAction:
-                        bestAction = actionSet
-                        maxAction = total
-                curr[block[0]][block[1]] = discount * maxAction
-                currMoves[block[0]][block[1]] = bestAction[0][3]
-
-        values.append(curr)
-        moves.append(currMoves)
-
-
-        if converged(values[t], values[t - 1]):
-            break
-
-        t += 1
-    for i in range(len(mazeWorld)):
-        for j in range(len(mazeWorld[i])):
-            if values[t-1][i][j] == -999:
-                print "~~~W~~~".ljust(6) + "|",
-            else:
-                print str(values[t-1][i][j]).ljust(6)[0:6] + " |",
-        print ""
-
-    for i in range(len(mazeWorld)):
-        for j in range(len(mazeWorld[i])):
-            move = moves[t-1][i][j]
-            if mazeWorld[i][j] is not 0:
-                if mazeWorld[i][j] == -999:
-                    print "W  |",
-                else:
-                    print str(mazeWorld[i][j])[0:2].ljust(3) + "|",
-            elif move == LEFT:
-                print "<- |",
-            elif move == RIGHT:
-                print "-> |",
-            elif move == UP:
-                print "^  |",
-            else:
-                print "v  |",
-        print ""
-
-def converged(first, second):
-    thresh = 0.0001
-    for i in range(len(first)):
-        for j in range(len(first[i])):
-            if abs(first[i][j] - second[i][j]) > thresh:
-                return False
-    return True
-
-def getSurroundingBlocks(block):
-    blocks = list()
-    yA = block[0]
-    xA = block[1]
-
-    for y in range(yA - 1, yA + 2):
-        for x in range(xA - 1, xA + 2):
-            if x == xA and y == yA:
-                continue
-            if y < len(mazeWorld) and y >= 0 and x < len(mazeWorld[0]) and x >= 0:
-                if mazeWorld[y][x] is 0 and mazeWorld[y][x] is not -999:
-                    blocks.append((y, x))
-
-    return blocks
-
-"""
-Only return valid actions
-"""
-def getActions(node):
-    actions = list()
-    x = node[1]
-    y = node[0]
-
-    actions.append([(y - 1, x, intended, UP), (y, x + 1, right), (y, x - 1, left)])
-    actions.append([(y + 1, x, intended, DOWN), (y, x + 1, right), (y, x - 1, left)])
-    actions.append([(y, x + 1, intended, RIGHT), (y - 1, x, right), (y + 1, x, left)])
-    actions.append([(y, x - 1, intended, LEFT), (y - 1, x, right), (y + 1, x, left)])
-
-    return actions
+    print "\n\n~~~~~~~~~~ Reinforcement Learning ~~~~~~~~~~\n\n"
+    reinforcementLearning()
 
 if __name__ == "__main__":
     main()
